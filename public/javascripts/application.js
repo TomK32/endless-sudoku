be0e9bdbafbe28bedf7048bfa5c3c1c2d97045f3
Thomas R. Koll <tomk32@tomk32.de>
2010-10-17 10:26:29 +0200

    Show error message on ajax error

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
+  $('#error').ajaxError(function(event) {
+    $(this).html('An error occurreced or your internet connection is cut. <a href="#" onclick="$(this).parent().hide();">x</a>').show();
+  });
+
+
   b = new Board('board123', 'board');
   b.populate([{id: 'sudoku1', lat: 0, lng: 0, rows: ["023456789", "234560091", "345678023", "406789123", "567891034", "678912345", "789123456", "891234567", "912300678"]}
     ])
   b.draw();
-})
+});
