85704f07ad4b5440c3007facafcc0f9f6dfc623a
Thomas R. Koll <tomk32@tomk32.de>
2010-10-18 21:35:23 +0200

    Added mail smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Wieners::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
@@ -48,4 +48,13 @@ Wieners::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'endless-sudoku.heroku.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address  => "ananasblau.com",
+    :port  => 25,
+    :user_name  => "sudoku@ananasblau.com",
+    :password  => "929b26fd",
+    :authentication  => :login
+  }
 end
